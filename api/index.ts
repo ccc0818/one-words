e8050d12faf8d6e19b27b612f1359b991c5e6d0c
ccc0818 <cc.chn@qq.com>
2023-05-01 15:54:24 +0800

    build: 第一次提交

---
+++ api/index.ts
@@ -0,0 +1,58 @@
+async function getWords() {
+	return uni.request({
+		url: 'https://api.vvhan.com/api/ian?type=json',
+		method: 'GET'
+	});
+}
+
+async function getImage() {
+	return uni.request({
+		url: 'https://api.vvhan.com/api/view?type=json',
+		method: 'GET'
+	});
+}
+
+export async function getInfo() {
+	let info = getCache();
+	if (info) {
+		let res: IInfo = JSON.parse(info);
+		if (!isOut(res.date)) return res;
+	}
+
+	try {
+		const [words, image]: any[] = await Promise.all([getWords(), getImage()]);
+		let res: IInfo = {
+			words: words.data.data.vhan,
+			source: words.data.data.source,
+			author: words.data.data.creator,
+			image: image.data.imgurl,
+			date: new Date().toDateString(),
+		};
+
+		saveCache(res);
+
+		return res;
+	} catch {
+		throw new Error('获取数据错误');
+	}
+}
+
+interface IInfo {
+	words: string;
+	source: string;
+	author: string;
+	image: string;
+	date: string;
+}
+const cacheKey = 'info';
+function saveCache(obj: IInfo) {
+	return uni.setStorageSync(cacheKey, JSON.stringify(obj));
+}
+
+function getCache() {
+	return uni.getStorageSync(cacheKey);
+}
+
+function isOut(date: string) {
+	return !(new Date().toDateString() === date);
+}
---
+++ pages/index/index.vue
@@ -0,0 +1,103 @@
+<template>
+	<view class="home-container">
+		<image :src="bgRef" mode="heightFix" class="bg"></image>
+		<view class="words-container">
+			<text class="words">
+				{{ wordsRef }}\n
+				<text class="author">{{ authorRef }}</text>
+			</text>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getInfo } from '@/api';
+
+const bgRef = ref<string>('');
+const wordsRef = ref<string>('');
+const authorRef = ref<string>('');
+
+getInfo().then(res => {
+	bgRef.value = res.image;
+	wordsRef.value = res.words;
+	authorRef.value = `--${res.author} 《${res.source}》`;
+})
+
+</script>
+
+<style lang="scss" scoped>
+.home-container {
+	width: 100vw;
+	height: 100vh;
+	position: relative;
+	overflow: hidden;
+
+	.bg {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		pointer-events: none;
+	}
+
+	.words-container {
+		$color: #fff;
+		position: absolute;
+		width: 100%;
+		height: 50%;
+		bottom: 0;
+		backdrop-filter: blur(32px);
+		box-sizing: border-box;
+		padding: 40rpx;
+
+		.words {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			height: 80%;
+			max-width: 80%;
+			box-sizing: border-box;
+			padding: 40rpx;
+			writing-mode: vertical-rl;
+			letter-spacing: 10rpx;
+			font-size: 32rpx;
+			line-height: 64rpx;
+			color: $color;
+			mix-blend-mode: difference;
+
+			&::before,
+			&::after {
+				content: '';
+				width: 30%;
+				height: 50%;
+				border: 4rpx solid $color;
+				position: absolute;
+			}
+
+			&::before {
+				border-left-color: transparent;
+				border-bottom-color: transparent;
+				top: 0;
+				right: 0;
+			}
+
+			&::after {
+				border-right-color: transparent;
+				border-top-color: transparent;
+				bottom: 0;
+				left: 0;
+			}
+
+			.author {
+				font-size: 24rpx;
+				display: inline-block;
+				height: 100%;
+				text-align: right;
+			}
+		}
+	}
+}
+</style>
